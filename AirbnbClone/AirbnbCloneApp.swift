ae769d57b8ffa5b1d97a06cec197fcddfc68dff4
esse.jacques.co <essedansomon@gmail.com>
2024-05-28 13:14:47 +0000

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Esse Jacques  on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirbnbClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirbnbClone
+//
+//  Created by Esse Jacques  on 28/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
